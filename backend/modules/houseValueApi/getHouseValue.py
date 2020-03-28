c403e3cdceaa3c8025e5e716681ea4e0a9e82b2a
catherinelee274 <catherinelee274@yahoo.com>
2020-03-28 16:29:45 -0700

    error handling if add doesnt exist

--- backend/modules/houseValueApi/getHouseValue.py
+++ backend/modules/houseValueApi/getHouseValue.py
@@ -26,35 +26,43 @@ gmaps = googlemaps.Client(key=gmapKey)
 reverse_geocode_result = gmaps.reverse_geocode((lat, long))
 # print(reverse_geocode_result)
 # data = json.loads(reverse_geocode_result)
-# example output: 642 N Martel Ave, Los Angeles, CA 90036, USA
+results = reverse_geocode_result[1]
+if 'formatted_address' in results:
+        formatted_add = reverse_geocode_result[1]['formatted_address']
+        # example output: 642 N Martel Ave, Los Angeles, CA 90036, USA
+        
+        split = formatted_add.split(', ')
 
-formatted_add = reverse_geocode_result[1]['formatted_address']
+        streetAddress = split[0]
+        city = split[1]
+        stateZip = split[2].split()
+        state = stateZip[0]
+        zipCode = stateZip[1]
 
-split = formatted_add.split(', ')
+        # print(formatted_add)
+        # streetAddress = '7018 Hawthorn Ave'
+        # city = 'Los Angeles'
+        # state = 'CA'
+        # zipcode = 90028
 
-streetAddress = split[0]
-city = split[1]
-stateZip = split[2].split()
-state = stateZip[0]
-zipCode = stateZip[1]
+        data = {'token':estatedKey,
+                'street_address':streetAddress,
+                'city':city,
+                'state':state,
+                'zip_code':zipCode}
 
-# need to handle case 
+        r = requests.get(url = "https://apis.estated.com/v4/property", params = data)
+        response = r.json()
+        response = response['data']
+        if 'assessments' in response:
+                assessments = response['assessments']
+                # print(assessments)
+                # full assessments is [{'year': 2019, 'land_value': 240757, 'improvement_value': 82576, 'total_value': 323333}]
+                print(assessments[0]['land_value'])
+        else:
+                print("Assessments not available for this location")
+else:
+        print("An address does not exist here")
 
-# print(formatted_add)
-# streetAddress = '7018 Hawthorn Ave'
-# city = 'Los Angeles'
-# state = 'CA'
-# zipcode = 90028
-
-data = {'token':estatedKey,
-        'street_address':streetAddress,
-        'city':city,
-        'state':state,
-        'zip_code':zipCode}
-
-
-r = requests.get(url = "https://apis.estated.com/v4/property", params = data)
-response = r.json()
-assessments = response['data']['assessments']
-print(assessments[0]['land_value'])
-# print(r.json())
+        
+        
