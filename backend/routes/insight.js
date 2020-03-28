0dc70dd0d57c7dbc5e604647a049495c23defd4c
Ronak Shah <ronakshahultimate@gmail.com>
2020-03-28 03:09:13 -0700

    Connected Tree API

--- backend/routes/insight.js
+++ backend/routes/insight.js
@@ -2,22 +2,47 @@ const express = require('express');
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', (req, res, next) => {
-    const lat = req.params.lat;
-    const lng = req.params.lng;
-    const type = req.params.type;
-    const radius = req.params.radius;
-    const squareFootage = req.params.squareFootage;
-    const occupancy = req.params.occupancy;
-    const height = req.params.height;
+router.get('/', async (req, res, next) => {
+    const lat = req.query.lat;
+    const lng = req.query.lng;
+    const type = req.query.type;
+    const radius = req.query.radius;
+    const squareFootage = req.query.squareFootage;
+    const occupancy = req.query.occupancy;
+    const height = req.query.height;
 
-    res.status(200);
-    res.json({
-        rating: "A",
-        jobs: -300,
-        trees: -50,
-        carbon: 500
+    let getTreeData = new Promise((success, nosuccess) => {
+
+        const { spawn } = require('child_process');
+        const pyprog = spawn('python', ['./modules/treeApi/treeFind.py', lat, lng, radius]);
+    
+        pyprog.stdout.on('data', function(data) {
+    
+            success(data);
+        });
+    
+        pyprog.stderr.on('data', (data) => {
+    
+            nosuccess(data);
+        });
     });
+
+    try {
+        let trees = await getTreeData;
+
+        res.status(200);
+        res.json({
+            rating: "A",
+            jobs: -300,
+            trees: parseInt(trees.toString()),
+            carbon: 500
+        });
+    } catch(err) {
+        res.status(500);
+        res.json({
+            error: err
+        })
+    }
 });
 
 module.exports = router;
