8482d330227af3f7473dfb7c87a33ebc054a901a
catherinelee274 <catherinelee274@yahoo.com>
2020-03-28 02:15:54 -0700

    :bug: YYYYY

--- backend/treeApi/treeFind.py
+++ backend/treeApi/treeFind.py
@@ -1,19 +1,28 @@
-import pandas as pd        # reading in data files, data cleaning
+import pandas as pd
+from flask import Flask, request, jsonify
+import numpy as np
 
+app = Flask(__name__)
 
 @app.route("/tree", methods=['GET'])
-def findTrees(inputX, inputY, radius):
-    if request.method == 'GET': 
-        inputX = request.inputX # change if needed 
-        inputY = request.inputY # change if needed 
-        radius = request.radius # change if needed 
-        data = pd.read_csv("trees.csv")
-        arr = data[['X','Y']].to_numpy()
-        arr[:,0] = (inputX-arr[:,0] )**2
-        arr[:, 1] = (inputY-arr[:,1])**2
-        added = arr[:,0] + arr[:,1]
-        added = (added)**(1/2)
-        treeCount = added[added <= radius].shape[0]
-        return treeCount
-
-
+def findTrees():
+	# if request.method == 'GET':
+	inputX = request.args.get('X') # change if needed 
+	inputY = request.args.get('Y') # change if needed 
+	radius = request.args.get('rad') # change if needed 
+	# inputX = -18
+	# inputY = 30
+	# radius = 0.003
+	data = pd.read_csv("trees.csv")
+	arr = data[['X','Y']].to_numpy()
+	# xArr = np.full((635558, 1), inputX)
+	# yArr = np.full((635558, 1), inputY)
+	arr[:,0] = (inputX-arr[:,0])**2
+	arr[:, 1] = (inputY-arr[:,1])**2
+	added = arr[:,0] + arr[:,1]
+	added = (added)**(1/2)
+	treeCount = added[added <= radius].shape[0]
+	# return treeCount
+	return jsonify(treeCount)
+	# print(treeCount)
+# findTrees()
