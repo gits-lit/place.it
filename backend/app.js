090880e18fb4a6ecf6ad3bef3229fb2183440185
Ronak Shah <ronakshahultimate@gmail.com>
2020-03-29 04:11:21 -0700

    Backend Works with Frontend

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -10,6 +11,7 @@ var insightRouter = require('./routes/insight');
 
 var app = express();
 
+app.use(cors())
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
